e41165ed8d1a34f6054277b7a7276625fceedcda
Nidjat <gurbanli.nidjat001@gmail.com>
2022-11-15 12:37:31 +0400

    adding theme switcher

--- src/ts/phones.ts
+++ src/ts/phones.ts
@@ -4,11 +4,16 @@ $(function f()
 {
     let $currentCategory = $("#nav");
     let $buyButton = $(".submit-button");
+    let $switcher = $(".theme-switcher");
 
     $currentCategory.on("mouseover", () =>
     {
       //  alert("Hello");
     });
 
+    $switcher.on("click", () => {
+        $switcher.val("Dark theme");
+        alert("Hello");
+    });
 
 });
--- src/ts/wearables.ts
+++ src/ts/wearables.ts
@@ -4,9 +4,15 @@ $(function f()
 {
     let $currentCategory = $("#nav");
     let $buyButton = $(".submit-button");
+    let $switcher = $(".theme-switcher");
 
     $currentCategory.on("mouseover", () =>
     {
       //  alert("Hello");
+    });
+
+    $switcher.on("click", () => {
+        $switcher.val("Dark theme");
+        alert("Hello");
     });
 });

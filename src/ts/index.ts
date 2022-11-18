0122eee21cb1707b1f15e0714570f84653c36c2e
Nidjat <gurbanli.nidjat001@gmail.com>
2022-11-18 19:25:13 +0400

    changing index.html

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,6 +4,9 @@ $(function f()
 {
     let $searchBox = $("#search-box").val();
     let $searchingButton = $("#searching-button");
+    let $scrollTopBUtton = $(".scrolltopButton");
+    let $switcher = $(".theme-switcher");
+    let counter: number = 0;
 
     $searchingButton.on("click", () => 
     {
@@ -17,4 +20,37 @@ $(function f()
             crossDomain: true,
         });
     });
+
+    
+    $switcher.on("click", () => {
+        if (counter === 0)
+        {
+            $switcher.val("Dark theme");
+            alert("Dark");
+            ++counter;
+        }
+        else
+        {
+            $switcher.val("Light theme");
+            alert("Light");
+            --counter;
+        }
+    });
+
+    $scrollTopBUtton.on("click", () => {
+        $('body,html').animate({ scrollTop: 0}, 1500);
+      });
+    
+      $(window).on("scroll", () => {
+        let scrolled = $(window).scrollTop();
+    
+        if(scrolled > 100) 
+        {
+            $scrollTopBUtton.fadeIn();
+        } 
+        else 
+        {
+            $scrollTopBUtton.fadeOut();
+        }
+      });
 });

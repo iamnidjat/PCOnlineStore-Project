219ca1439dbc6a98b67429ecaff1204dc6bc7189
Nidjat <gurbanli.nidjat001@gmail.com>
2022-11-18 13:47:19 +0400

    some changes

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,5 +2,19 @@ import $ from "jquery";
 
 $(function f()
 {
-    
+    let $searchBox = $("#search-box").val();
+    let $searchingButton = $("#searching-button");
+
+    $searchingButton.on("click", () => 
+    {
+        $.ajax({
+            url: ``,
+            success: (data) =>
+            {
+                //window.open("file2.html")
+            },
+            method: "GET",
+            crossDomain: true,
+        });
+    });
 });
